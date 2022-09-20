34a1096797c298d9162b1f216078056d77d44ddb
leenduati <leenduati21@gmail.com>
2022-09-20 06:49:10 +0000

    Fixed task 11 to for stderr to output directly

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys as lee
-lee.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-lee.stderr.write("1")
+lee.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 lee.exit(1)
 
