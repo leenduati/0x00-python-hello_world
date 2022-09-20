d9882af0a89aff7c05b8f303d8d9db7f28ec2669
leenduati <leenduati21@gmail.com>
2022-09-20 19:39:50 +0000

    Fixed task 9 to take care of no >0

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def print_last_digit(number):
-	if number < 0:
-		number = number * -1
-	sol = number % 10
-	print(sol)
-	return sol
+    if number < 0:
+        number = number * -1
+    sol = number % 10
+    print(sol, end="")
+    return sol
