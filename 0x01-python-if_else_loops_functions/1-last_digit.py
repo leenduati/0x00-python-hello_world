2974324e1c68e43a80ce69b835d702675f1e5d71
leenduati <leenduati21@gmail.com>
2022-09-20 18:22:58 +0000

    Added pycode styleto some tasks

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,23 +4,25 @@ number = random.randint(-10000, 10000)
 neg_num = 0
 sol = 0
 
-# initialize sol(to accept the last num) and neg_num(to cater for negative numbers)
+# initialize sol(to accept the last num) and neg_num(to cater for -ve no
 
 # a function to fulfill conditions necessary
+
+
 def check_num(n):
-	if n > 5:
-		print(f'Last digit of {number} is {n} and is greater than 5')
-	elif n == 0:
-		print(f'Last digit of {number} is {n} and is 0')
-	else:
-		print(f'Last digit of {number} is {n} and is less than 6 and not 0')
+    if n > 5:
+        print(f'Last digit of {number} is {n} and is greater than 5')
+    elif n == 0:
+        print(f'Last digit of {number} is {n} and is 0')
+    else:
+        print(f'Last digit of {number} is {n} and is less than 6 and not 0')
 
 
 # call the function
 if number < 0:
-	neg_num = number * -1
-	sol = neg_num % 10
-	check_num(-sol)
+    neg_num = number * -1
+    sol = neg_num % 10
+    check_num(-sol)
 else:
-	sol = number % 10
-	check_num(sol)
+    sol = number % 10
+    check_num(sol)
