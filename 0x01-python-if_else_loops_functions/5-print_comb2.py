984918c0907344d837a700df807cd300333184d0
leenduati <leenduati21@gmail.com>
2022-09-20 16:26:59 +0000

    tasks 10 9 12 and 11

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for i in range(0,101):
+	if i < 10:
+		print(f'0{i}, ', end=" ")
+	elif 100 > i:
+		printf(f'{i}, ', end=" ")
+
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def print_last_digit(number):
+	if number < 0:
+		number = number * -1
+	sol = number % 10
+	print(sol)
+	return sol
