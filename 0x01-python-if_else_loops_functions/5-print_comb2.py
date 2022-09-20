bd70c928f2233002af33a94b45c48673176f2631
leenduati <leenduati21@gmail.com>
2022-09-20 21:14:16 +0000

    Task 5, printing all numbers btwn 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(0,101):
-	if i < 10:
-		print(f'0{i}, ', end=" ")
-	elif 100 > i:
-		printf(f'{i}, ', end=" ")
-
+for i in range(0, 99):
+    print("{0:02d}".format(i), end=", ")
+print("{:d}".format(99))
