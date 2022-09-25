4cd8b5c8f0af4d9bf6018a935a3dc31cec12b545
leenduati <leenduati21@gmail.com>
2022-09-25 15:32:51 -0400

    took care of when index is exact as length

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@
 def element_at(my_list, idx):
     len_of_list = len(my_list)
 
-    if idx > len_of_list or idx < 0:
+    if idx >= len_of_list or idx < 0:
         return None
     else:
         return my_list[idx]
