290d9ef45174dd74fd7bc2601a59dbd83a11e451
leenduati <leenduati21@gmail.com>
2022-09-25 15:51:38 -0400

    added to check if list exists

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,7 @@
 
 def print_reversed_list_integer(my_list=[]):
     i = -1
-    while (i >= -len(my_list)):
-        print('{:d}'.format(my_list[i]))
-        i -= 1
+    if my_list:
+        while (i >= -len(my_list)):
+            print('{:d}'.format(my_list[i]))
+            i -= 1
