14e39dc6f77c78493fa47bc99237cb72c430e021
leenduati <leenduati21@gmail.com>
2022-09-25 15:59:29 -0400

    task 4, coping list to list in python

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if my_list:
+        copy_list = my_list[:]
+        if idx < 0 or idx >= len(my_list):
+            return copy_list
+        else:
+            my_list[idx] = element
+            return copy_list
