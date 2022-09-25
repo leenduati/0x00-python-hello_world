3b71471cc5daacae53bcdf44eaea4ee08bf3f61c
leenduati <leenduati21@gmail.com>
2022-09-25 17:07:19 -0400

    task 5, run code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    if my_string:
-        for i in my_string:
-            if i == 'c' or i == 'C':
-                my_list = list(my_string)
-                idx = my_list.index(i)
-                return my_string[:idx] + my_string[idx + 1:]
+    new_str = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str = new_str + char
+    return new_str
