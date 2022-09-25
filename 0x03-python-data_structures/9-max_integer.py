c0a49972892f2259613f399e81d904656612d53f
leenduati <leenduati21@gmail.com>
2022-09-25 17:59:15 -0400

    added greater or equal to in task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,6 +5,6 @@ def max_integer(my_list=[]):
         return None
     max_num = 0
     for i in my_list:
-        if i > max_num:
+        if i >= max_num:
             max_num = i
     return max_num
