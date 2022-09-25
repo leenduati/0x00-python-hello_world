af996620ece4b539ad1e2f39be9353c1f63c5cc0
leenduati <leenduati21@gmail.com>
2022-09-25 16:02:34 -0400

    task 4, modifed the correct list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,5 +6,5 @@ def new_in_list(my_list, idx, element):
         if idx < 0 or idx >= len(my_list):
             return copy_list
         else:
-            my_list[idx] = element
+            copy_list[idx] = element
             return copy_list
