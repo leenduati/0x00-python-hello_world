1dc4ff7df1fd3b982913ec61a96bf0bf10d8e0ab
leenduati <leenduati21@gmail.com>
2022-09-25 16:36:32 -0400

    task 5, remove at certain index

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin.python3
+
+def no_c(my_string):
+    if my_string:
+        for i in my_string:
+            if i == 'c' or i == 'C':
+                my_list = list(my_string)
+                idx = my_list.index(i)
+                return my_string[:idx] + my_string[idx + 1:]
