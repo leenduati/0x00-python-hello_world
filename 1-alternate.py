6d00d8fd49cf4faabf0b9e3fa94059765102ae27
leenduati <leenduati21@gmail.com>
2022-10-11 16:58:17 -0400

    fixed task 1, and created personal sol under 1-alt.py for pld review

--- 1-alternate.py
+++ 1-alternate.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
+
 def safe_print_integer(value):
     try:
+        res = value.isnumeric()
+    except (AttributeError):
         print("{:d}".format(value))
-    except (ValueError, TypeError):
-        return False
-    else:
         return True
+    else:
+        return False
