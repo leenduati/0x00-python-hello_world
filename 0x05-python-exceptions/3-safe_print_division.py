f7d18cefd985d1ae6498da8b2f8c83ee9b2dc452
leenduati <leenduati21@gmail.com>
2022-10-11 22:28:34 -0400

    removed comments in task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,10 +4,8 @@ def safe_print_division(a, b):
     res = None
     try:
         res = a / b
-        # print("{:d}".format(res))
     except ZeroDivisionError:
         return res
     finally:
         print("Inside result : {}".format(res))
-        # print("{}".format(res))
         return res
