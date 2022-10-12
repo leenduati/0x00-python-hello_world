405e8a4cf3930f76f732c5b39e879764ef083dd0
leenduati <leenduati21@gmail.com>
2022-10-11 22:29:51 -0400

    removed whitespace in return value

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return res
     finally:
-        print("Inside result : {}".format(res))
+        print("Inside result: {}".format(res))
         return res
