fb5c6fcd556ceee3bd698396ac4136ed5c640d00
leenduati <leenduati21@gmail.com>
2022-10-11 22:20:15 -0400

    declared res first in task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    res = None
     try:
         res = a / b
         # print("{:d}".format(res))
     except ZeroDivisionError:
-        res = None
+        return res
     finally:
         print("Inside result : {}".format(res))
         # print("{}".format(res))
