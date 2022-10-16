1da485179b8613cf1fdbec8a88400a52ff04e088
leenduati <leenduati21@gmail.com>
2022-10-16 10:54:57 -0400

    Task 1, square class with attributes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Empty class square"""
+
+
+class Square():
+    """
+    Square class which is self-explanatory with a __init__ class
+    """
+    def __init__(self, size):
+        """ __init__ method__
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
