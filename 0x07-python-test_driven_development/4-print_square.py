84808c416e6036784bc9874c419a80a2da3d635b
leenduati <leenduati21@gmail.com>
2022-10-21 08:04:26 -0400

    Fixed print_ssquare(0) command in task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -19,7 +19,7 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     if size == 0:
-        print("")
+        return
     if size == 1:
         print("#")
     if size > 1:
