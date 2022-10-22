831941e48ca6aa5debac11f0dbcc78b24ec104c1
leenduati <leenduati21@gmail.com>
2022-10-22 07:50:57 -0400

    Task 10,function that returns a key with the biggest integer value.

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    max_val = 0
+    if a_dictionary is None:
+        return None
+    for i in a_dictionary.items():
+        if i[1] >= max_val:
+            # print(i[1])
+            max_val = i[1]
+        if i[0] is None or i[1] is None:
+            return None
+    key_list = list(a_dictionary.keys())
+    val_list = list(a_dictionary.values())
+    pos = val_list.index(max_val)
+    return key_list[pos]
