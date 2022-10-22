12b1ab70dfd115ff0da114c6de9a8b2bf661f83b
leenduati <leenduati21@gmail.com>
2022-10-22 05:52:31 -0400

    Task 3amd task 4 on sets 0x04

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    list_1 = list(set_1)
-    list_2 = list(set_2)
-
-    for i in list_1:
-        if i in list_2:
-            return set(i)
+    return set_1 & set_2
