e0473aaf0c36506d75a5737b50c3fe3af91b30e5
leenduati <leenduati21@gmail.com>
2022-10-22 16:29:22 -0400

    Task 13, added conditions

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,13 @@
 
 
 def weight_average(my_list=[]):
+    if my_list is None or len(my_list) == 0:
+        return 0
     count = 0
     total = 0
     for i in my_list:
         total = total + (i[0] * i[1])
         count = count + (i[1])
+    if count == 0:
+        return 0
     return total / count
