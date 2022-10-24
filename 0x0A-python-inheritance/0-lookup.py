e60fc7dbd42fbc16dcda99c6b38ba6f57f345180
leenduati <leenduati21@gmail.com>
2022-10-24 19:32:49 -0400

    returns the list of available attributes and methods of an object:

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#/usr/bin/python3
+
+""" This will return allatts and methods of an object """
+
+
+def lookup(obj):
+    """ Attributes and methods of an obk=ject are returned
+    using this function """
+    return dir(obj)
