4265b9f6a948b54c089613e5dc3ce29e8c567b01
leenduati <leenduati21@gmail.com>
2022-10-25 14:57:39 -0400

    fn that reads a text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" function that opens and reads a filename"""
+
+
+def read_file(filename=""):
+    """read_file is a filename that opens and reads a text file """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
