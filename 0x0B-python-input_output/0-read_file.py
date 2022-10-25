4faf45b0e3962ccb28c42c42906ee40281ad1104
leenduati <leenduati21@gmail.com>
2022-10-25 15:10:20 -0400

    added r tag in file 0, and fixed pycodestyle errors

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """read_file is a filename that opens and reads a text file """
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
     f.close()
