78ce5a613910e28129fdafa5b361eb62cc749502
leenduati <leenduati21@gmail.com>
2022-11-01 15:09:14 -0400

    Fixed pycodestyle linter errors int task0

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 
+
 """Class Base:
 private class attribute __nb_objects = 0
 class constructor: def __init__(self, id=None):"""
 
+
 class Base:
     """ first class called Base with an __int.py__"""
+
     __nb_objects = 0
+
     def __init__(self, id=None):
-        """if id is not None, assign the public instance attribute id with this argument
+        """if id is not None, assign the public instance att id with this arg
          value - you can assume id is an integer"""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
-           Base.__nb_objects += 1
-           self.id = Base.__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
